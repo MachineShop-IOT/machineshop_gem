81269b4fb81f0cdd81ff61a4d27f51e0032963a8
Ryan Stephens <ryan@mach19.com>
2013-03-09 19:01:11 -0700

    Fix Machineshop vs MachineShop inconsistencies

--- machineshop/spec/lib/api_calls_spec.rb
+++ machineshop/spec/lib/api_calls_spec.rb
@@ -4,7 +4,7 @@ MachineShop.api_base= 'http://machineshop.dev:3000'
 MachineShop.auth_token= 'ssn6JsULgrjwZxCmaP9n'
 
 describe MachineShop do
-  it "should get a device" do
+  it "should get all devices for th user" do
     element_data = MachineShop.get_device
     puts "Element Data: " + element_data.to_s
     element_data.should_not be_nil    

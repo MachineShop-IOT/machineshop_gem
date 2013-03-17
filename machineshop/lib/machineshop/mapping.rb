f64c642994dba00be952fc99619f238efd1a1649
Ryan Stephens <ryan@mach19.com>
2013-03-17 16:39:35 -0600

    implemented google and utility endpoints

--- machineshop/lib/machineshop/mapping.rb
+++ machineshop/lib/machineshop/mapping.rb
@@ -1,6 +1,34 @@
 module MachineShop
-  class Mapping < APIResource
-  # Specific API calls
+  class Mapping
+    # Specific API calls
+    def self.geocode(params={}, auth_token)
+      MachineShop.get(geocode_url, auth_token, params)
+    end
 
+    def self.directions(params={}, auth_token)
+      MachineShop.get(directions_url, auth_token, params)
+    end
+
+    def self.distance(params={}, auth_token)
+      MachineShop.get(distance_url, auth_token, params)
+    end
+
+    private
+
+    def self.geocode_url
+      url + '/geocode/json'
+    end
+
+    def self.directions_url
+      url + '/directions/json'
+    end
+
+    def self.distance_url
+      url + '/distance/json'
+    end
+
+    def self.url
+      '/platform/google'
+    end
   end
 end
--- machineshop/lib/machineshop/utility.rb
+++ machineshop/lib/machineshop/utility.rb
@@ -1,5 +1,26 @@
 module MachineShop
-  class Utility < APIResource
+  class Utility
+    def self.email(params={}, auth_token)
+      MachineShop.post(email_url, auth_token, params)
+    end
+
+    def self.sms(params={}, auth_token)
+      MachineShop.post(sms_url, auth_token, params)
+    end
+
+    private
+
+    def self.email_url
+      url + '/payload_fields'
+    end
+
+    def self.sms_url
+      url + '/SMS'
+    end
+
+    def self.url
+      '/platform/utility'
+    end
 
   end
 end

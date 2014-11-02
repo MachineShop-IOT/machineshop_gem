229750197bc2abc6d11af447e2abe9047170e593
Gaurub Pandey <gp@bajratechnologies.com>
2014-11-02 17:38:58 +0545

    adding some debugging functionality

--- machineshop/lib/machineshop/api_operations/delete.rb
+++ machineshop/lib/machineshop/api_operations/delete.rb
@@ -3,6 +3,7 @@ module MachineShop
     module Delete
       def delete
         response = MachineShop.gem_delete(url, @auth_token,{})
+        puts response
         refresh_from(response, @auth_token)
         self
       end

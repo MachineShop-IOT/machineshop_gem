c8eaff76be24f263b2c44f246b47f147f2b1518f
Gaurub Pandey <gp@bajratechnologies.com>
2014-11-02 15:52:21 +0545

    Adding new method

--- machineshop/lib/machineshop/data_source_types.rb
+++ machineshop/lib/machineshop/data_source_types.rb
@@ -6,14 +6,19 @@ module MachineShop
     include MachineShop::APIOperations::Delete
 
 
-     def create_data_source(params)
+    def create_data_source(params)
       params.merge!({:data_source_type => self.id})
       DataSources.create(params, @auth_token)
     end
 
-     def create_email_data_source(params)
+    def create_email_data_source(params)
       params.merge!({:data_source_type => self.id})
       MachineShop.gem_post(email_data_source_url, @auth_token, params)
+    end
+
+    def self.update(id,auth_token,params={})
+      response = MachineShop.gem_put(self.url+"/#{id}", auth_token, params)
+      Util.convert_to_machineshop_object(response, auth_token, self.class_name)
     end
 
     private

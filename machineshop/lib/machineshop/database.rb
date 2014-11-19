d60e36ce4b0b07c69235be579879a690db6ca79b
Niroj Sapkota <niroj@bajratechnologies.com>
2014-11-19 16:13:26 +0545

    connecting to database testing

--- machineshop/lib/machineshop/database.rb
+++ machineshop/lib/machineshop/database.rb
@@ -17,16 +17,29 @@ module MachineShop
       # @db_connected=true
       begin
 
+        establish_connection ActiveRecord::Base.configurations[Rails.env]
 
-        ActiveRecord::Base.establish_connection(
-          Rails.configuration.database_configuration[Rails.env]
 
-        # adapter:  'mysql', # or 'postgresql' or 'sqlite3'
-        # host:     MachineShop.configuration.db_host,
-        # database: MachineShop.configuration.db_name,
-        # username: MachineShop.configuration.db_username,
-        # password: MachineShop.configuration.db_password,
-        )
+
+  #       ActiveRecord::Base.establish_connection(
+  #         # Rails.configuration.database_configuration[Rails.env]
+
+  #         adapter: 'sqlite3',
+  # database: 'db/development.sqlite3',
+  # pool: '5',
+  # timeout: '5000'
+
+  # # -----------------------------------
+
+
+  #       # adapter:  'mysql', # or 'postgresql' or 'sqlite3'
+  #       # host:     MachineShop.configuration.db_host,
+  #       # database: MachineShop.configuration.db_name,
+  #       # username: MachineShop.configuration.db_username,
+  #       # password: MachineShop.configuration.db_password,
+
+  #       # -----------------------------
+  #       )
 
       rescue ActiveRecord::AdapterNotSpecified =>e
         puts "yaha1"
@@ -40,7 +53,8 @@ module MachineShop
       rescue Exception => e
         puts "yaha4"
         # @db_connected=false
-        raise DatabaseError.new("Connection to Database refused "+e)
+        # raise DatabaseError.new("Connection to Database refused "+e)
+        raise DatabaseError.new(e)
         # load schema file
       end
 

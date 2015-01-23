1c8ef8bb4e05d6204d434480d8a2f3ddc534815e
sajana <sajana@bajratechnologies.com>
2015-01-23 18:27:35 +0545

    put for authorized route edit

--- machineshop/lib/machineshop/routes.rb
+++ machineshop/lib/machineshop/routes.rb
@@ -7,6 +7,14 @@ module MachineShop
 	      auth_routes = MachineShop.gem_get("/platform/authorized_routes/", auth_token, nil)
 	    end
 
+	    def self.single_authorized_route(id,auth_token)      
+	      auth_routes = MachineShop.gem_get("/platform/authorized_routes/"+id, auth_token, nil)
+	    end
+
+	    def self.single_authorized_route_put(id,json,auth_token)      
+	      auth_routes = MachineShop.gem_put("/platform/authorized_routes/"+id, auth_token, json)
+	    end
+
 		private
 		def self.url
 			'/platform/routes?flag=portal'

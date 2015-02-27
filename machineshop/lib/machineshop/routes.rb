78a85fa6d842b355dab893318658e97d35bc8f11
Umesh <umesh@bajratechnologies.com>
2015-02-27 11:37:25 +0545

    route list fix

--- machineshop/lib/machineshop/routes.rb
+++ machineshop/lib/machineshop/routes.rb
@@ -2,6 +2,7 @@
 module MachineShop
 
 	class Routes < APIResource
+		include MachineShop::APIOperations::List
 
 		def self.authorized_routes_json(auth_token)      
 	      auth_routes = MachineShop.gem_get("/platform/authorized_routes/", auth_token, nil)
@@ -11,7 +12,7 @@ module MachineShop
 	      auth_routes = MachineShop.gem_get("/platform/authorized_routes/"+id, auth_token, nil)
 	    end
 
-	    def self.single_authorized_route_put(id,json,auth_token)      
+	    def self.single_authorized_route_put(id,json,auth_token)   
 	      auth_routes = MachineShop.gem_put("/platform/authorized_routes/"+id, auth_token, json)
 	    end
 
